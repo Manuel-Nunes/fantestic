6f6a23a9a18b80430c55526487ee614787042682
Johan Smit <johansm@bbd.co.za>
2023-09-28 11:57:10 +0200

    beforeEach working

--- src/screens/AppMain.cy.jsx
+++ src/screens/AppMain.cy.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import AppMain from './AppMain'
-import { beforeEach } from 'mocha';
 
 describe('<AppMain />', () => {
-  before('renders', () => {
+  beforeEach('renders', () => {
     cy.mount(<AppMain />)
   });
 
